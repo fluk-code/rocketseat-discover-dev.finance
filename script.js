01a16c2fd45ab62604023d433a517d7db2f7a256
Felipe Ferreira <ferreirafelipericardo@gmail.com>
2021-03-18 23:48:33 -0300

    :pencil: part two: added assets/images.svg, styles-responsive.css, changed index.html and styles.csss

---
+++ script.js
@@ -0,0 +1,12 @@
+const Modal = {
+    open(){
+        document
+            .querySelector('div.modal-overlay')
+            .classList.add('active');
+    },
+    close(){
+        document
+            .querySelector('div.modal-overlay')
+            .classList.remove('active');
+    }
+}
